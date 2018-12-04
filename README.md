4530fafaa715dd77218be7359193b9b8cf842a82
Jay Hui <kkhuiaa@gmail.com>
2018-12-05 00:37:43 +0800

    updating

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# msbd5001
+# MSBD5001-Fall2018
+
+In class competition for MSBD 5001
+
+## Getting Started
+To run the script, you may type the following in terminal:
+```
+python main.py
+```
+
+### Prerequisites
+
+The script is written in python 3.6.3 with required packages:
+
+```
+os
+time
+pickle
+warnings
+pathlib2==2.3.0
+numpy==1.14.0
+pandas==0.23.4
+scikit-learn==0.20.1
+keras==2.2.4
+xgboost==0.7.post4
+matplotlib==2.1.1
+```
+
+### Installing
+os, time, pickle, warnings should be installed already when installing python
+
+For installing specific version of package, you can install it as follow: (through pip)
+
+```
+pip install -Iv pandas==0.23.4
+```
+
+## Running the tests
+
+The main.py can be run as normal .py file. If you want to test another testing file, please upload another testing data in the main() function.
+
+### And coding style tests
+
+The scipt follows [pip8](https://www.python.org/dev/peps/pep-0008/) standard python notation.
+
+## Authors
+* **Hui Kwat Kong** - *20123133* - *kkhuiaa*
--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-#%%
 """
 Step 1:
 """
-
 
 import time
 import pickle as p
 import numpy as np
 import pandas as pd
-import warnings
 
 from sklearn.preprocessing import Imputer, StandardScaler
 from sklearn.metrics import make_scorer, mean_squared_error
@@ -24,27 +21,46 @@ from keras.wrappers.scikit_learn import KerasRegressor
 from keras.constraints import max_norm
 from keras.optimizers import SGD, Adam
 from keras.layers.normalization import BatchNormalization
-
 from keras import regularizers
 
 from xgboost import XGBRegressor
 from script import categorytoordinal, feature_selection_by_rfe
-random_state = 37 #for re-production
-
-train0 = pd.read_csv('data/all/train.csv')
-target = 'time'
-test = pd.read_csv('data/all/test.csv')
-samplesubmission = pd.read_csv('data/all/samplesubmission.csv')
-train = train0.drop(target, axis=1)
-y_train = train0[target]
 
 def copy_data(df, times=16):
+    """Copy the data several times
+    
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        input data
+    times : int, optional
+        the number for copying
+    
+    Returns
+    -------
+    pandas.DataFrame
+        the number of row will be len(df)*times
+    """
+
     out_df = df.copy()
     for i in range(times-1):
         out_df = df.append(out_df)
     return out_df
 
 def create_feature(df):
+    """Create the feature like log, sqrt, power, multiplication, division exponential etc
+    
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        input data
+    
+    Returns
+    -------
+    pandas.DataFrame
+        the transformed data
+    """
+
     df.drop(['id', 'random_state'], axis=1, inplace=True)
     df['n_jobs'] = np.where(df['n_jobs']==-1, 10, df['n_jobs'])
     numerator_list_before = ['n_samples', 'max_iter', 'n_features', 'n_informative', 'n_classes', 'n_clusters_per_class']
@@ -80,98 +96,147 @@ def create_feature(df):
         df[col] = df[col].astype(float)
     return df
 
-cto = categorytoordinal.CategoryToOrdinal(other_threshold=1)
-cto.fit(train[['penalty']], y_train)
-train = train.append(copy_data(test, 16))
-train_transformed = cto.transform(train)
-train_transformed2 = create_feature(df=train_transformed)
+def main(train0, test, random_state=37):
+    """The main script in applying the create_feature and build the DNN model, output the result as kkhuiaa_20123133_prediction
+    
+    Parameters
+    ----------
+    train0 : pandas.DataFrame
+        the input traning data
+    test : pandas.DataFrame
+        the input testing data
+    random_state : int, RandomState instance or None, optional
+        If int, random_state is the seed used by the random number generator; If RandomState instance, random_state is the random number generator; If None, the random number generator is the RandomState instance used by np.random.
+    
+    Returns
+    -------
+    csv
+        kkhuiaa_20123133_prediction.csv, which is the prediction result on test.csv
+    """
+    target = 'time'
+    train = train0.drop(target, axis=1) #drop the target in training data
+    y_train = train0[target] #y label
 
-sc1 = StandardScaler()
-sc1.fit(train_transformed2)
-train_scalez1 = pd.DataFrame(data=sc1.transform(train_transformed2), index=train_transformed2.index, columns=train_transformed2.columns)
-X_train = train_scalez1[:400]
-print(X_train.shape)
+    cto = categorytoordinal.CategoryToOrdinal(other_threshold=1) #transform the categorical variables to ordinal variables
+    cto.fit(train[['penalty']], y_train)
+    train = train.append(copy_data(test, 16)) #Copy the test so that the standardization will remember the testing feature more
+    train_transformed = cto.transform(train)
+    train_transformed2 = create_feature(df=train_transformed)
 
-from genlib.ml import feature_selection_by_rfe as fsr
-feature_table_rfe = fsr.feature_selection_by_rfe(X_train, y_train, [XGBRegressor]
-    , cv=3, n_jobs=-1, scoring=make_scorer(mean_squared_error, greater_is_better=False)
-    , random_state=random_state, plot_directory='all/evaluation/rfe/', plot_file_name='rfe_433', show_plot=True)
-print(feature_table_rfe[0]) #table
-print(feature_table_rfe[1]) #dict
+    sc1 = StandardScaler() #standardize the data
+    sc1.fit(train_transformed2)
+    train_scalez1 = pd.DataFrame(data=sc1.transform(train_transformed2), index=train_transformed2.index, columns=train_transformed2.columns)
+    X_train = train_scalez1[:400]
+    print(X_train.shape) #(400, 3849)
 
-train_rfe = train_scalez1[feature_table_rfe[0][feature_table_rfe[0]['XGBRegressor_rank'] <= 25].index.tolist()]
-print(train_rfe.shape)
+    from genlib.ml import feature_selection_by_rfe as fsr
+    feature_table_rfe = fsr.feature_selection_by_rfe(X_train, y_train, [XGBRegressor]
+        , cv=3, n_jobs=-1, scoring=make_scorer(mean_squared_error, greater_is_better=False)
+        , random_state=random_state, plot_directory='evaluation/rfe/', plot_file_name='rfe_433', show_plot=True)
+    print(feature_table_rfe[0]) #table
+    print(feature_table_rfe[1]) #dict {'XGBRegressor': 11}
 
-pca = PCA(n_components=.99)
-pca.fit(train_rfe)
-train_pca = pd.DataFrame(data=pca.transform(train_rfe), index=train_rfe.index, columns=['pca_'+str(i+1) for i in range(0, pca.n_components_)])
+    train_rfe = train_scalez1[feature_table_rfe[0][feature_table_rfe[0]['XGBRegressor_rank'] <= 25].index.tolist()]
+    print(train_rfe.shape) #(2000, 25)
 
-sc2 = StandardScaler()
-sc2.fit(train_pca)
-X = pd.DataFrame(data=sc2.transform(train_pca), index=train_pca.index, columns=train_pca.columns)
-X_cols = X.columns.tolist()
-X_train = X[X_cols][:400]
-X_test = X[X_cols][-100:]
-print(X_train.shape)
+    pca = PCA(n_components=.99)
+    pca.fit(train_rfe)
+    train_pca = pd.DataFrame(data=pca.transform(train_rfe), index=train_rfe.index, columns=['pca_'+str(i+1) for i in range(0, pca.n_components_)])
 
-#%%
-seed = random_state
-def create_baseline(dropout_rate=0.3, l2=0, optimizer='Adam', activation='elu', maxnorm=5):
-    model = Sequential() # create model
-    model.add(Dense(30, input_dim=len(X_train.columns), activation='elu', kernel_regularizer=regularizers.l2(l2)))
-    model.add(BatchNormalization())
-    model.add(Dropout(dropout_rate))
-    model.add(Dense(60, activation='elu', kernel_regularizer=regularizers.l2(l2)))
-    model.add(BatchNormalization())
-    model.add(Dropout(dropout_rate))
-    model.add(Dense(45, activation='elu', kernel_regularizer=regularizers.l2(l2)))
-    model.add(BatchNormalization())
-    model.add(Dropout(dropout_rate))
-    model.add(Dense(30, activation='elu', kernel_regularizer=regularizers.l2(l2)))
-    model.add(BatchNormalization())
-    model.add(Dropout(dropout_rate))
-    model.add(Dense(15, activation='elu', kernel_regularizer=regularizers.l2(l2)))
-    model.add(BatchNormalization())
-    model.add(Dropout(dropout_rate))
-    model.add(Dense(1))
-    model.compile(loss='mean_squared_error', optimizer=optimizer, metrics=['mse'])
-    return model
-model = KerasRegressor(build_fn=create_baseline, batch_size=round(len(X_train)/4), epochs=500)
-np.random.seed(seed)
+    sc2 = StandardScaler()
+    sc2.fit(train_pca)
+    X = pd.DataFrame(data=sc2.transform(train_pca), index=train_pca.index, columns=train_pca.columns)
+    X_cols = X.columns.tolist()
+    X_train = X[X_cols][:400]
+    X_test = X[X_cols][-100:]
+    print(X_train.shape) #(400, 20)
 
-param = dict(epochs=[500], l2=[.1, .2, .3, .4], dropout_rate=[.05, .1])
+    seed = random_state
+    def create_baseline(dropout_rate=0.3, l2=0, optimizer='Adam', activation='elu', maxnorm=5):
+        model = Sequential() # create model
+        model.add(Dense(30, input_dim=len(X_train.columns), activation='elu', kernel_regularizer=regularizers.l2(l2)))
+        model.add(BatchNormalization()) #Normalize the activations of the previous layer at each batch, i.e. applies a transformation that maintains the mean activation close to 0 and the activation standard deviation close to 1.
+        model.add(Dropout(dropout_rate)) #drop_rate avoid overfitting
+        model.add(Dense(60, activation='elu', kernel_regularizer=regularizers.l2(l2)))
+        model.add(BatchNormalization())
+        model.add(Dropout(dropout_rate))
+        model.add(Dense(45, activation='elu', kernel_regularizer=regularizers.l2(l2)))
+        model.add(BatchNormalization())
+        model.add(Dropout(dropout_rate))
+        model.add(Dense(30, activation='elu', kernel_regularizer=regularizers.l2(l2)))
+        model.add(BatchNormalization())
+        model.add(Dropout(dropout_rate))
+        model.add(Dense(15, activation='elu', kernel_regularizer=regularizers.l2(l2)))
+        model.add(BatchNormalization())
+        model.add(Dropout(dropout_rate))
+        model.add(Dense(1))
+        model.compile(loss='mean_squared_error', optimizer=optimizer, metrics=['mse'])
+        return model
+    model = KerasRegressor(build_fn=create_baseline, batch_size=round(len(X_train)/4), epochs=500)
+    np.random.seed(seed)
 
-gridsearch = GridSearchCV(model, param_grid=param, cv=3, n_jobs=1, scoring=make_scorer(mean_squared_error, greater_is_better=False), verbose=0)
+    param = dict(epochs=[500], l2=[.1, .2, .3, .4], dropout_rate=[.05, .1])
 
-t0 = time.time()
-gridsearch.fit(X_train, y_train, verbose=0, return_train_score=True)
-t1 = time.time()
+    gridsearch = GridSearchCV(model, param_grid=param, cv=3, n_jobs=1, scoring=make_scorer(mean_squared_error, greater_is_better=False), verbose=0, return_train_score=True)
 
-gridsearch_best = gridsearch.best_estimator_
-early_stop_crit = EarlyStopping(monitor='val_loss', min_delta=0, patience=2000, verbose=0, mode='auto')
-history = gridsearch_best.fit(X_train, y_train, epochs=50000, batch_size=400, verbose=0, validation_split=0.1, shuffle=True, callbacks=[early_stop_crit])
+    t0 = time.time() #record the time
+    gridsearch.fit(X_train, y_train, verbose=0)
+    t1 = time.time()
 
-final_model = gridsearch_best
-print('time:', round(t1-t0, 2)) #calculate the time for gridsearch
-print('best params:', gridsearch.best_params_)
+    gridsearch_best = gridsearch.best_estimator_
+    early_stop_crit = EarlyStopping(monitor='val_loss', min_delta=0, patience=2000, verbose=0, mode='auto')
+    history = gridsearch_best.fit(X_train, y_train, epochs=50000, batch_size=400, verbose=0, validation_split=0.1, shuffle=True, callbacks=[early_stop_crit])
 
-best_position = gridsearch.best_index_
-print('best train score:', -gridsearch.cv_results_['mean_train_score'][best_position])
-print('best train std:', gridsearch.cv_results_['std_train_score'][best_position])
-print('best test score:', -gridsearch.cv_results_['mean_test_score'][best_position])
-print('best test std:', gridsearch.cv_results_['std_test_score'][best_position])
+    final_model = gridsearch_best
+    print('time:', round(t1-t0, 2)) #calculate the time for gridsearch
+    print('best params:', gridsearch.best_params_)
+    best_position = gridsearch.best_index_
+    print('best train score:', -gridsearch.cv_results_['mean_train_score'][best_position])
+    print('best train std:', gridsearch.cv_results_['std_train_score'][best_position])
+    print('best test score:', -gridsearch.cv_results_['mean_test_score'][best_position])
+    print('best test std:', gridsearch.cv_results_['std_test_score'][best_position])
 
-train_predict = pd.Series(final_model.predict(X_train))
-print('train:', train_predict.describe())
-print('mse:', mean_squared_error(y_train, train_predict))
+    # time: 871.08
+    # best params: {'dropout_rate': 0.1, 'epochs': 500, 'l2': 0.3}
+    # best train score: 1.2951904441069788
+    # best train std: 0.4152040843407351
+    # best test score: 4.347559117067388
+    # best test std: 1.364786447247397
+    # train: count   400.00
 
-print('=================================================================================================')
-print('=================================================================================================')
+    train_predict = pd.Series(final_model.predict(X_train))
+    print('train:', train_predict.describe())
+    # train: count   400.00
+    # mean      2.91
+    # std       5.24
+    # min      -0.38
+    # 25%       0.33
+    # 50%       1.25
+    # 75%       3.32
+    # max      41.00
+    # dtype: float64
+    print('mse:', mean_squared_error(y_train, train_predict))
+    # mse: 0.7610716710240856
 
-submission = test[['id']]
-submission.loc[:, 'time_p'] = final_model.predict(X_test)
-submission.loc[:, 'time_p'] = np.where(submission['time_p']<0, submission[submission['time_p']>0]['time_p'].min(), submission['time_p'])
-print(submission['time_p'].describe())
+    print('=================================================================================================')
+    print('=================================================================================================')
 
-submission = submission.rename(columns={'time_p': 'time'})
-submission.to_csv('all/kkhuiaa_20123133_prediction_22.csv', index=False)
+    submission = test[['id']].copy()
+    submission.loc[:, 'time_p'] = final_model.predict(X_test)
+    submission.loc[:, 'time_p'] = np.where(submission['time_p']<0, submission[submission['time_p']>0]['time_p'].min(), submission['time_p'])
+    print(submission['time_p'].describe())
+    # count   100.00
+    # mean      5.43
+    # std       8.45
+    # min       0.01
+    # 25%       0.36
+    # 50%       1.85
+    # 75%       6.85
+    # max      42.40
+    # Name: time_p, dtype: float64
+
+    submission = submission.rename(columns={'time_p': 'time'})
+    submission.to_csv('kkhuiaa_20123133_prediction.csv', index=False)
+
+if __name__ == "__main__":
+    main(train0=pd.read_csv('data/train.csv'), test = pd.read_csv('data/test.csv'))
